c02ff3d2e98a3f553b5abe32492f8fb7045365b6
nuix09 <136358529+nuix09@users.noreply.github.com>
2024-09-10 01:47:41 +0530

    Fixed some issues with slash commands

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,11 @@
-const { SlashCommandBuilder } = require(discord.js);
+const { SlashCommandBuilder } = require("discord.js");
 
-module.exports = {
-  data: new SlashCommandBuilder.setName("ping").setDescription(
-    "Replies with a Pong!"
-  ),
-  async execute(interaction) {
-    await interaction.reply("Pong!");
-  },
-};
+const data = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("Replies with a Pong!");
+
+async function execute(interaction) {
+  await interaction.reply("Pong!");
+}
+
+module.exports = { data, execute };
