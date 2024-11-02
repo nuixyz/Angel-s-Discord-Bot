7c0a7c4c439d69f58cb231b9d095f75c7b18ebed
nuix09 <136358529+nuix09@users.noreply.github.com>
2024-11-02 17:09:49 +0530

    Added an intro command

---
+++ commands/utility/intro.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { introduction } = require("../../src/GIFs");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("intro")
+    .setDescription("About the bot"),
+
+  async execute(interaction) {
+    const intro = introduction.message;
+
+    const introEmbed = new EmbedBuilder()
+      .setColor("#FF69B4")
+      .setDescription(intro)
+      .setImage(
+        "https://media1.tenor.com/m/IlSZ5q9UyMcAAAAd/shy-blush-blush.gif"
+      );
+    await interaction.reply({ embeds: [introEmbed] });
+  },
+};
